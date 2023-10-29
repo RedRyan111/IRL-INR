df2ac78058d53c0170dcefe805b054996e4627f6
RedRyan111 <ravenengineerryan@gmail.com>
2023-10-29 01:10:18 -0500

    refactor

--- models.py
+++ models.py
@@ -1,10 +1,7 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from matplotlib import pyplot as plt
-from sklearn.manifold import TSNE
-import torchvision.models as models
-import time
+
 
 class ResidLinear(nn.Module):
     def __init__(self, n_in, n_out, activation=nn.Tanh):
@@ -26,7 +23,7 @@ class Encoder(nn.Module):
 
         layers = [nn.Linear(n, hidden_dim),
                   activation(),
-                 ]
+                  ]
         for _ in range(1, num_layers):
             if resid:
                 layers.append(ResidLinear(hidden_dim, hidden_dim, activation=activation))
@@ -46,127 +43,6 @@ class Encoder(nn.Module):
         return z
 
 
-# class BasicBlock(nn.Module):
-#     expansion = 1
-
-#     def __init__(self, in_planes, planes, stride=1, is_last=False):
-#         super(BasicBlock, self).__init__()
-#         self.is_last = is_last
-#         self.conv1 = nn.Conv2d(in_planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
-#         self.bn1 = nn.BatchNorm2d(planes)
-#         self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=1, padding=1, bias=False)
-#         self.bn2 = nn.BatchNorm2d(planes)
-
-#         self.shortcut = nn.Sequential()
-#         if stride != 1 or in_planes != self.expansion * planes:
-#             self.shortcut = nn.Sequential(
-#                 nn.Conv2d(in_planes, self.expansion * planes, kernel_size=1, stride=stride, bias=False),
-#                 nn.BatchNorm2d(self.expansion * planes)
-#             )
-
-#     def forward(self, x):
-#         out = F.relu(self.bn1(self.conv1(x)))
-#         out = self.bn2(self.conv2(out))
-#         out += self.shortcut(x)
-#         preact = out
-#         out = F.relu(out)
-#         if self.is_last:
-#             return out, preact
-#         else:
-#             return out
-
-
-# class Bottleneck(nn.Module):
-#     expansion = 4
-
-#     def __init__(self, in_planes, planes, stride=1, is_last=False):
-#         super(Bottleneck, self).__init__()
-#         self.is_last = is_last
-#         self.conv1 = nn.Conv2d(in_planes, planes, kernel_size=1, bias=False)
-#         self.bn1 = nn.BatchNorm2d(planes)
-#         self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
-#         self.bn2 = nn.BatchNorm2d(planes)
-#         self.conv3 = nn.Conv2d(planes, self.expansion * planes, kernel_size=1, bias=False)
-#         self.bn3 = nn.BatchNorm2d(self.expansion * planes)
-
-#         self.shortcut = nn.Sequential()
-#         if stride != 1 or in_planes != self.expansion * planes:
-#             self.shortcut = nn.Sequential(
-#                 nn.Conv2d(in_planes, self.expansion * planes, kernel_size=1, stride=stride, bias=False),
-#                 nn.BatchNorm2d(self.expansion * planes)
-#             )
-
-#     def forward(self, x):
-#         out = F.relu(self.bn1(self.conv1(x)))
-#         out = F.relu(self.bn2(self.conv2(out)))
-#         out = self.bn3(self.conv3(out))
-#         out += self.shortcut(x)
-#         preact = out
-#         out = F.relu(out)
-#         if self.is_last:
-#             return out, preact
-#         else:
-#             return out
-
-
-# class ResNet(nn.Module):
-#     def __init__(self, block, num_blocks, in_channel=3, zero_init_residual=False):
-#         super(ResNet, self).__init__()
-#         self.in_planes = 64
-
-#         self.conv1 = nn.Conv2d(1, 64, kernel_size=3, stride=1, padding=1,
-#                                bias=False)
-#         self.bn1 = nn.BatchNorm2d(64)
-#         self.layer1 = self._make_layer(block, 64, num_blocks[0], stride=1)
-#         self.layer2 = self._make_layer(block, 128, num_blocks[1], stride=2)
-#         self.layer3 = self._make_layer(block, 256, num_blocks[2], stride=2)
-#         self.layer4 = self._make_layer(block, 512, num_blocks[3], stride=2)
-#         self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
-
-#         for m in self.modules():
-#             if isinstance(m, nn.Conv2d):
-#                 nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
-#             elif isinstance(m, (nn.BatchNorm2d, nn.GroupNorm)):
-#                 nn.init.constant_(m.weight, 1)
-#                 nn.init.constant_(m.bias, 0)
-
-#         # Zero-initialize the last BN in each residual branch,
-#         # so that the residual branch starts with zeros, and each residual block behaves
-#         # like an identity. This improves the model by 0.2~0.3% according to:
-#         # https://arxiv.org/abs/1706.02677
-#         if zero_init_residual:
-#             for m in self.modules():
-#                 if isinstance(m, Bottleneck):
-#                     nn.init.constant_(m.bn3.weight, 0)
-#                 elif isinstance(m, BasicBlock):
-#                     nn.init.constant_(m.bn2.weight, 0)
-
-#     def _make_layer(self, block, planes, num_blocks, stride):
-#         strides = [stride] + [1] * (num_blocks - 1)
-#         layers = []
-#         for i in range(num_blocks):
-#             stride = strides[i]
-#             layers.append(block(self.in_planes, planes, stride))
-#             self.in_planes = planes * block.expansion
-#         return nn.Sequential(*layers)
-
-#     def forward(self, x):
-#         out = F.relu(self.bn1(self.conv1(x)))
-#         out = F.relu(self.conv1(x))
-#         out = self.layer1(out)
-#         out = self.layer2(out)
-#         out = self.layer3(out)
-#         out = self.layer4(out)
-#         out = self.avgpool(out)
-#         out = torch.flatten(out, 1)
-#         return out
-
-# def resnet50():
-#     backbone = models.__dict__['resnet50']()
-#     backbone.fc = nn.Identity()
-#     return backbone
-
-
 class FunctionRepresentation(nn.Module):
     """Function to represent a single datapoint. For example this could be a
     function that takes pixel coordinates as input and returns RGB values, i.e.
@@ -180,9 +56,10 @@ class FunctionRepresentation(nn.Module):
         final_non_linearity (torch.nn.Module): Final non linearity to use.
             Usually nn.Sigmoid() or nn.Tanh().
     """
+
     def __init__(self, feature_dim, layer_sizes, num_frequencies=128):
         super(FunctionRepresentation, self).__init__()
-        self.feature_dim = feature_dim # 3 or 1
+        self.feature_dim = feature_dim  # 3 or 1
         self.layer_sizes = layer_sizes
         self.non_linearity = nn.ReLU()
         self.num_frequencies = num_frequencies
@@ -214,7 +91,6 @@ class FunctionRepresentation(nn.Module):
             coordinates (torch.Tensor): Shape (batch_size, coordinate_dim)
         """
         # print('coordinates.shape: ', coordinates.shape)
-        start_time = time.time()
         features = coordinates
         for i in range(len(weights)):
             features = F.linear(features, weights[i], biases[i])
@@ -223,8 +99,6 @@ class FunctionRepresentation(nn.Module):
             else:
                 features = self.non_linearity(features)
         features = features.squeeze()
-        end_time = time.time()
-        # print(start_time - end_time)
         return features
 
     def get_weight_shapes(self):
@@ -277,6 +151,7 @@ class FunctionRepresentation(nn.Module):
         return FunctionRepresentation(self.feature_dim,
                                       self.layer_sizes).to(device)
 
+
 class HyperNetwork(nn.Module):
     """Hypernetwork that outputs the weights of a function representation.
     Args:
@@ -285,6 +160,7 @@ class HyperNetwork(nn.Module):
         layer_sizes (tuple of ints): Specifies size of each hidden layer.
         non_linearity (torch.nn.Module):
     """
+
     def __init__(self, function_representation, latent_dim, hypernet_layer_sizes):
         super(HyperNetwork, self).__init__()
         self.function_representation = function_representation
@@ -373,4 +249,3 @@ class HyperNetwork(nn.Module):
         """
         output = self.forward_layers(latents)
         return self.output_to_weights(output)
-
